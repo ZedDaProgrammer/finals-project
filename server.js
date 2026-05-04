336082c511341ceb1cefba4b83dbd7018040206f
@ZedDaProgrammer <alexzedrick07@gmail.com>
2026-05-04 14:27:42 +0800

    Initialized server

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send('API is fucking running');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is madafaking running ${PORT}`);
+});
