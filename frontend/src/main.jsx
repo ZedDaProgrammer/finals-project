34d60c3556bb820f7710f71d2ba46604447eeae9
@ZedDaProgrammer <alexzedrick07@gmail.com>
2026-05-14 22:56:39 +0800

    VERY SMALL CHANGE

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from '../context/AuthContext';
-import { FeedbackProvider } from '../context/FeedbackContext'; // <-- Import it here
+import { FeedbackProvider } from '../context/feedbackContext'; // <-- Import it here
 import App from './App';
 import '../style.css';
 
